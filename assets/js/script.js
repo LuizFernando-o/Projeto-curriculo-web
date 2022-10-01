5658e1c080358f87bcc1012ac2231c69812691a2
luizfernando-o <luiz.2019230186@unicap.br>
2022-10-01 11:57:34 -0300

    ajustes básicos, inserção de links.

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,7 +49,27 @@ themeButton.addEventListener('click', () => {
 })
 
 
+/* :::::::::::::: Efeito de Fade In na página ao ser carregada :::::::::::::: */
 
+var opacity = 0;
+var intervalID = 0;
+window.onload = fadeIn;
+
+function fadeIn() {
+    setInterval(show, 50);
+}
+
+function show() {
+    var body = document.getElementById("body");
+    opacity = Number(window.getComputedStyle(body)
+        .getPropertyValue("opacity"));
+    if (opacity < 1) {
+        opacity = opacity + 0.05;
+        body.style.opacity = opacity
+    } else {
+        clearInterval(intervalID);
+    }
+}
 
 
 
